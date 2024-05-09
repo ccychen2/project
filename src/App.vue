437467dd043ae2f3a0faa76bcc6f4b50fd31ee2b
nick <ccychen2@gmail.com>
2024-05-09 11:41:15 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>i am app</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = 'i fuck you'
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+const ap = () => {
+  console.log('i love you')
+}
+
+ap()
