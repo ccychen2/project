6aa8a478820e722e8ce5c66a0d912a8f4de1bb42
nick <ccychen2@gmail.com>
2024-05-11 17:00:45 +0800

    feat: add commend

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <template>
   <div>
-    <h1>i am app</h1>
-
-
+    <h1> add commend</h1>
   </div>
-
 </template>
 
 <script setup lang="ts">
 
-
-
-
-
-
-const str = "i fuck you";
-
 for (let i = 0; i < 100; i++) {
-
-
-  console.log(i);;
-
-
-
+  console.log(i)
 }
 </script>
 
